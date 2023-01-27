49e23a4b9d48b8811aceec648891dbc1eb7b7558
Quinten Krauth <quintenkrauth19@gmail.com>
2023-01-27 18:20:00 -0500

    interactive title font added

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -1,21 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
 const HeroSection = () => {
-    const sectionStyle = {
-        background: `linear-gradient(190deg, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url(https://gfjules.com/wp-content/uploads/2015/08/gluten-free-white-cake-on-plate-gfJules-720x405.jpg)`
-    };
+  const sectionStyle = {
+    background: `linear-gradient(190deg, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url(https://gfjules.com/wp-content/uploads/2015/08/gluten-free-white-cake-on-plate-gfJules-720x405.jpg)`,
+  };
 
-    return (
-        <section className="hero" style={sectionStyle}>
-            <h2>Try one of our Best Sellers
-                <br />
-                <span className="big-title">
-                    Choco Woko
-                </span>
-            </h2>
-            <button>Find one you Love</button>
-        </section>
-    )
+  const [selected, setSelected] = useState("");
+  const array = [
+    "Angel Cake",
+    "Beer Cake",
+    "Babka",
+    "Baumkuchen",
+    "Carrot Cake",
+    "Chiffon Cake",
+    "Esterhazy Torte",
+    "Faworki",
+    "Fig Cake",
+    "Karpatka",
+    "Lamington",
+    "Lemon Cake",
+    "Moon Cake",
+    "Tiramisu",
+    "Yule Log",
+  ];
+
+  const handleclick = () => {
+    const selected = Math.floor(Math.random() * array.length);
+    setSelected(array[selected]);
+  };
+
+  return (
+    <section className="hero" style={sectionStyle}>
+      <h2>
+        Try one of our Best Sellers
+        <br />
+        <span className="big-title">{selected}</span>
+      </h2>
+      <button onClick={handleclick}>Find one you Love</button>
+    </section>
+  );
 };
 
 export default HeroSection;
