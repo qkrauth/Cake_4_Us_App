a1fb89ac1d8993a18410f3b0ecc53aa59ccbd85a
Quinten Krauth <quintenkrauth19@gmail.com>
2023-02-13 17:51:12 -0500

    deployment changes

--- server/index.js
+++ server/index.js
@@ -2,17 +2,27 @@ const express = require("express");
 const cors = require("cors");
 const server = express();
 const db = require("./util/database");
+const {PORT} = process.env;
+const path = require("path");
 const { Base, Extra } = require("./util/models");
 const seed = require("./seed/seed");
 const {getOptions} = require("./controllers/cakeController");
 
+require("dotenv").config();
+
 server.use(express.json());
 server.use(cors());
 
+server.use(express.static(path.resolve(__dirname, "../build")));
+
 server.get("/api/getOptions", getOptions);
 
-// db
-//     .sync({force: true})
-//     .then(() => seed());
+server.get("/*", function (req, res) {
+    res.sendFile(path.join(__dirname, "..build", "index.html"))
+});
 
-server.listen(4500, () => console.log("running on PORT 4500"));
+db
+    // .sync({force: true})
+    // .then(() => seed());
+
+server.listen(PORT, () => console.log(`server running on port ${PORT}`));
